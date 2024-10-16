3a254fe3f07319eb9de6a7dc51f3bdff645d5d2e
C04l <117013174+C04L@users.noreply.github.com>
2024-10-16 17:58:21 +0700

    fixing

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: "/",
-                destination: "/authentication",
+                destination: "/login",
                 permanent: true,
             },
         ];
